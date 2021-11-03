2718de8fdd8218bb0bfaeac0fde975283b6e4a91
Geeknerd1337 <geeknerd1337@gmail.com>
2021-11-02 22:21:04 -0500

    Initial Commit

---
+++ code/Hammer/SquatSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+
+[Hammer.EditorModel( "models/editor/playerstart.vmdl" )]
+[Hammer.EntityTool( "Squat Spawn", "Terry Squat Simulator", "Spawn point for squats at the gym" )]
+[Library( "tss_squat_spawn" )]
+public class SquatSpawn : AnimEntity
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+		Transmit = TransmitType.Always;
+	}
+}
